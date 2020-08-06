bed105318f3afec5c7fa8c2ec9025e7cd62c9e09
Kamil Paczkowski <37217644+ShamanDev@users.noreply.github.com>
2020-08-07 00:17:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const electron = require('electron')
-const { BrowserWindow, app } = require('electron')
+const electron = require('electron');
+const { BrowserWindow, app } = require('electron');
 
 function createWindow(){
     let win = new BrowserWindow({
         width: 500,
         height: 600,
+        minHeight:500,
+        minWidth:400,
+
         webPreferences: {
             nodeIntegration: true
         },
-        autoHideMenuBar: true
+        autoHideMenuBar: false
     })
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
