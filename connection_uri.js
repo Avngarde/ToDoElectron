ded301b23f4d057af7a30048c175f348e93e29dd
Kamil Paczkowski <37217644+Avngarde@users.noreply.github.com>
2020-08-09 18:23:04 +0200

    Add files via upload

---
+++ connection_uri.js
@@ -0,0 +1,7 @@
+const { models } = require("mongoose");
+
+const uri ='mongodb+srv://500PlusClient:SecretsOfVatican1978@cluster0-1kawz.azure.mongodb.net/ToDoElectronFlutter?retryWrites=true&w=majority';
+
+module.exports = {
+    uri: uri
+}
