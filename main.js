16ddb7cae63755572770f70dbfa91bb83bdf2f3a
Kamil Paczkowski <37217644+Avngarde@users.noreply.github.com>
2020-08-13 22:41:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ function createWindow(){
     let win = new BrowserWindow({
         width: 500,
         height: 600,
-        minHeight:500,
-        minWidth:400,
+        minHeight: 500,
+        minWidth: 400,
 
         webPreferences: {
             nodeIntegration: true
         },
-        autoHideMenuBar: false
+        autoHideMenuBar: true
     })
 
     win.loadFile('index.html');
